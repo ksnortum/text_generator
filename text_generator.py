826ea1a1a6f3d0c389f75d20051a47e561c425f6
Knute Snortum <knute@snortum.net>
2022-05-20 15:44:57 -0700

    Use just one class, defaultdict, Counter

--- text_generator.py
+++ text_generator.py
@@ -1,18 +1,17 @@
 from nltk import WhitespaceTokenizer
 from nltk.util import bigrams
-from markov_chain import MarkovChain
+from collections import Counter, defaultdict
 
 
 class TextGenerator:
 
     def __init__(self):
         self.bi_grams = []
-        self.markov_chain = MarkovChain()
+        self.markov_chain = {}
 
     def run(self):
         self.load_corpus()
         self.create_markov_chain()
-        # self.index_loop()
         self.head_loop()
 
     def load_corpus(self):
@@ -22,37 +21,35 @@ class TextGenerator:
             self.bi_grams = list(bigrams(tokens))
 
     def create_markov_chain(self):
-        for bi_gram in self.bi_grams:
-            self.markov_chain.add(bi_gram)
+        tails_by_head = defaultdict(list)
 
-    def index_loop(self):  # Not currently used
-        ans = input()
+        # make a list of all tails for this head
+        for head, tail in self.bi_grams:
+            tails_by_head[head].append(tail)
 
-        while ans != 'exit':
-            try:
-                bi_gram = self.bi_grams[int(ans)]
-                print(f'Head: {bi_gram[0]}\tTail: {bi_gram[1]}')
-            except IndexError:
-                print("Index Error. Please input an integer that is in the range of the corpus.")
-            except ValueError:
-                print("Value Error. Please input an integer.")
-            except TypeError:
-                print("Type Error. Please input an integer.")
-
-            ans = input()
+        # use the list of tails to create a Counter, then set the
+        # Markov chain using the frequency dict
+        for head, list_of_tails in tails_by_head.items():
+            tails = Counter(list_of_tails)
+            self.markov_chain[head] = tails.most_common()
 
     def head_loop(self):
-        # input()  # TODO why is this necessary when I run in the IDE?
         head = input()
 
         while head != 'exit':
-            if head in self.markov_chain.get_chain():
-                self.markov_chain.print_head_and_tails(head)
+            if head in self.markov_chain.keys():
+                self.print_head_and_tails(head)
             else:
                 print(f'Head: {head}')
                 print('Key Error. The requested word is not in the model. Please input another word.\n')
 
             head = input()
+
+    def print_head_and_tails(self, head: str) -> None:
+        print(f'Head: {head}')
+        for element in self.markov_chain[head]:
+            print(f'Tail: {element[0]}\tCount: {element[1]}')
+        print()
 
 
 if __name__ == "__main__":
